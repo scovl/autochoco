60855c2a7eb5c1b8168dcbb621933735bc0457fa
Vitor Lobo <lobocode@gmail.com>
2023-01-31 10:06:56 -0300

    add sudo

--- Linux/install-apps.sh
+++ Linux/install-apps.sh
@@ -6,20 +6,24 @@ echo "2. Fedora"
 echo "3. ArchLinux"
 read -p "Selecione uma opção (1/2/3): " os
 
+echo "Insira sua senha de administrador (sudo)"
+read -s password
+echo
+
 if [ $os -eq 1 ]; then
-  sudo apt update
+  echo $password | sudo -S apt update
   while read app; do
-    sudo apt install -y $app
+    echo $password | sudo -S apt install -y $app
   done < apps.txt
 elif [ $os -eq 2 ]; then
-  sudo dnf update
+  echo $password | sudo -S dnf update
   while read app; do
-    sudo dnf install -y $app
+    echo $password | sudo -S dnf install -y $app
   done < apps.txt
 elif [ $os -eq 3 ]; then
-  sudo pacman -Syu
+  echo $password | sudo -S pacman -Syu
   while read app; do
-    sudo pacman -S --noconfirm $app
+    echo $password | sudo -S pacman -S --noconfirm $app
   done < apps.txt
 else
   echo "Opção inválida"
